221594b12248c32a86b3e030e649a09fa469231f
Yaniv Zlotnik <yanivzlotnik1@gmail.com>
2024-05-19 18:51:04 +0300

    Rename folders "remote-state" and "setup"

--- setup/ecr.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ecr_repository" "my_ecr_repository" {
-  for_each = var.ecr_repositories
-
-  name = "${var.organization}-${var.name}-${each.value}"
-
-  image_tag_mutability = "IMMUTABLE"
-}
