233e9bae6766a375937332e58a096a78bc3a9c3c
Yaniv Zlotnik <yanivzlotnik1@gmail.com>
2024-05-19 19:40:45 +0300

    Update access_entries for the EKS cluster

--- 3-eks/main.tf
+++ 3-eks/main.tf
@@ -38,6 +38,7 @@ module "eks" {
   cluster_name                             = "${var.name}-eks"
   cluster_version                          = var.eks_cluster_version
   cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
