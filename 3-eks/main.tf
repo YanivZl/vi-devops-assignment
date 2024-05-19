6d1decadd6e4e37b8c606b88d12811dc7b502762
Yaniv Zlotnik <yanivzlotnik1@gmail.com>
2024-05-19 18:58:58 +0300

    Return EKS to basic

--- 3-eks/main.tf
+++ 3-eks/main.tf
@@ -22,8 +22,6 @@ module "vpc" {
 
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
-    # Tags subnets for Karpenter auto-discovery
-    "karpenter.sh/discovery" = var.name
   }
 
   tags = local.tags
@@ -53,37 +51,12 @@ module "eks" {
       min_size      = var.eks_intial_node_group_min_size
       max_size      = var.eks_intial_node_group_max_size
       desired_size  = var.eks_intial_node_group_desired_size
-
-      labels = {
-        # Used to ensure Karpenter runs on nodes that it does not manage
-        "karpenter.sh/controller" = "true"
-      }
-
-      taints = {
-        # The pods that do not tolerate this taint should run on nodes
-        # created by Karpenter
-        karpenter = {
-          key    = "karpenter.sh/controller"
-          value  = "true"
-          effect = "NO_SCHEDULE"
-        }
-      }
     }
   }
 
   cluster_addons = {
     coredns = {
-      configuration_values = jsonencode({
-        tolerations = [
-          # Allow CoreDNS to run on the same nodes as the Karpenter controller
-          # for use during cluster creation when Karpenter nodes do not yet exist
-          {
-            key    = "karpenter.sh/controller"
-            value  = "true"
-            effect = "NoSchedule"
-          }
-        ]
-      })
+      most_recent = true
     }
     kube-proxy = {
       most_recent = true
@@ -97,15 +70,9 @@ module "eks" {
     }
   }
 
-  enable_efa_support = true
 
 
-  tags = merge(local.tags, {
-    # NOTE - if creating multiple security groups with this module, only tag the
-    # security group that Karpenter should utilize with the following tag
-    # (i.e. - at most, only one security group should have this tag in your account)
-    "karpenter.sh/discovery" = var.name
-  })
+  tags = local.tags
 }
 
 ################################################################################
@@ -176,32 +143,6 @@ module "eks_blueprints_addons" {
     #   chart      = "reflector"
     #   version    = "7.1.262"
     #   namespace  = "kube-system"
-    # },
-    # karpenter = {
-    #   name       = "karpenter"
-    #   repository = "oci://public.ecr.aws/karpenter"
-    #   repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-    #   repository_password = data.aws_ecrpublic_authorization_token.token.password
-    #   chart               = "karpenter"
-    #   version             = "0.36.1"
-    #   wait                = false
-    #   namespace  = "kube-system"
-    #   values = [
-    #     <<-EOT
-    #     nodeSelector:
-    #       karpenter.sh/controller: 'true'
-    #     tolerations:
-    #       - key: CriticalAddonsOnly
-    #         operator: Exists
-    #       - key: karpenter.sh/controller
-    #         operator: Exists
-    #         effect: NoSchedule
-    #     settings:
-    #       clusterName: ${module.eks.cluster_name}
-    #       clusterEndpoint: ${module.eks.cluster_endpoint}
-    #       interruptionQueue: ${module.karpenter.queue_name}
-    #     EOT
-    #   ]
     # }
   }
 }
@@ -280,119 +221,3 @@ module "ebs_csi_driver_irsa" {
 #     }
 #   }
 # }
-
-################################################################################
-# Karpenter Controller & Node IAM roles, SQS Queue, Eventbridge Rules, EC2NodeClass
-################################################################################
-
-resource "helm_release" "karpenter" {
-  namespace           = "kube-system"
-  name                = "karpenter"
-  repository          = "oci://public.ecr.aws/karpenter"
-  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  repository_password = data.aws_ecrpublic_authorization_token.token.password
-  chart               = "karpenter"
-  version             = "0.36.1"
-  wait                = false
-
-  values = [
-    <<-EOT
-    nodeSelector:
-      karpenter.sh/controller: 'true'
-    tolerations:
-      - key: CriticalAddonsOnly
-        operator: Exists
-      - key: karpenter.sh/controller
-        operator: Exists
-        effect: NoSchedule
-    settings:
-      clusterName: ${module.eks.cluster_name}
-      clusterEndpoint: ${module.eks.cluster_endpoint}
-      interruptionQueue: ${module.karpenter.queue_name}
-    EOT
-  ]
-
-  lifecycle {
-    ignore_changes = [
-      repository_password
-    ]
-  }
-}
-
-module "karpenter" {
-  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
-  version = "~> 20.9"
-
-  cluster_name = module.eks.cluster_name
-
-  # Name needs to match role name passed to the EC2NodeClass
-  node_iam_role_use_name_prefix   = false
-  node_iam_role_name              = var.name
-  create_pod_identity_association = true
-
-  tags = local.tags
-}
-
-resource "kubectl_manifest" "karpenter_node_pool" {
-  depends_on = [
-    helm_release.karpenter
-  ]
-
-  yaml_body = <<-YAML
-    apiVersion: karpenter.sh/v1beta1
-kind: NodePool
-metadata:
-  name: default
-spec:
-  template:
-    spec:
-      nodeClassRef:
-        name: default
-      requirements:
-        - key: "karpenter.k8s.aws/instance-category"
-          operator: In
-          values: ["spot", "on-demand"]
-        - key: "karpenter.k8s.aws/capacity-type"
-          operator: In
-          values: ["c", "m", "r"]
-        - key: "karpenter.k8s.aws/instance-cpu"
-          operator: In
-          values: ["4", "8"]
-        - key: "karpenter.k8s.aws/instance-hypervisor"
-          operator: In
-          values: ["nitro"]
-        - key: "karpenter.k8s.aws/instance-generation"
-          operator: Gt
-          values: ["2"]
-  limits:
-    cpu: 1000
-  disruption:
-    consolidationPolicy: WhenEmpty
-    consolidateAfter: 30s
-  YAML
-}
-
-resource "kubectl_manifest" "karpenter_ec2_node_class" {
-  depends_on = [
-    helm_release.karpenter
-  ]
-
-  yaml_body = <<-YAML
-    apiVersion: karpenter.k8s.aws/v1beta1
-kind: EC2NodeClass
-metadata:
-  name: default
-spec:
-  amiFamily: AL2
-  role: ${var.name}
-  subnetSelectorTerms:
-    - tags:
-        karpenter.sh/discovery: ${var.name}
-  securityGroupSelectorTerms:
-    - tags:
-        karpenter.sh/discovery: ${var.name}
-  tags:
-    karpenter.sh/discovery: ${var.name}
-  YAML
-}
-
